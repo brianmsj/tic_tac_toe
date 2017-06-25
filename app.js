b9f3d865c083525ac5122376ca95ebb99d459541
brianmsj <brianmsj@gmail.com>
2017-06-25 13:01:44 -0700

    data representation on the baord

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 var state = {
-board: ["","","","","","","","",""],
+board: [0,0,0,0,0,0,0,0,0],
 turn: 0
 }
 
@@ -12,6 +12,14 @@ function changeTurns(state) {
     state.turn = 0
   }
 }
+function flipBoard(state,value) {
+  if(state.turn === 0) {
+     state.board[value] = 1
+  }
+  if(state.turn === 1) {
+    state.board[value] = 2
+  }
+}
 
 
 function eventListeners() {
@@ -19,14 +27,22 @@ function eventListeners() {
    if(state.turn === 0) {
      $(event.currentTarget).removeClass('white')
      $(event.currentTarget).addClass('red')
+     let index = $(event.currentTarget).attr('value')
+     flipBoard(state, index)
      changeTurns(state)
+     console.log(state)
    }
    else if(state.turn === 1) {
      $(event.currentTarget).removeClass('white')
      $(event.currentTarget).addClass('blue')
+     let index = $(event.currentTarget).attr('value')
+     flipBoard(state, index)
      changeTurns(state)
+     console.log(state)
    }
  })
+
+
 // if(state.turn === 0) {
 // $('.square').click(function(event) {
 //   $(event.currentTarget).addClass('red')

01b4b85c781ad53d7c843d968fe5f95e140726a8
brianmsj <brianmsj@gmail.com>
2017-06-25 20:54:18 -0700

    final product complete

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const state = {
 board: [0,0,0,0,0,0,0,0,0],
 turn: 0,
 winConditions: [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]],
-winner: ""
+winner: "",
+count: 0
 }
 
 function changeTurns(state) {
@@ -59,45 +60,61 @@ function winnerTwo(array) {
       return false;
     }
 }
+var stopProp = function (event) {
+  event.stopPropogation();
+}
+function lockButtons(state,stopProp) {
+  if(state.winner !=='') {
+  $('.square').on('click',stopProp)
+  $('.square').off('click');
+  }
+}
+
+function tieGame(state) {
+  if(state.count >= 9 && state.winner === "")
+  $('.playerWins').append('<h2 class=statewinner>Tie Game</h2>')
+}
 function displayWinner(state) {
   if(state.winner !== "") {
   $('.playerWins').append('<h2 class=statewinner>' + state.winner + '</h2>')
   }
 }
-function resetGame(state) {
-  state.board = [0,0,0,0,0,0,0,0,0],
-  state.turn = 0,
-  state.winner = ""
-  $('.square').removeClass('red')
-  $('.square').removeClass('blue')
-  $('.statewinner').remove()
-}
-
 
 function eventListeners() {
  $('.square').click(function(event) {
    if(state.turn === 0) {
+     state.count++
      $(event.currentTarget).removeClass('white')
      $(event.currentTarget).addClass('red')
+     $(event.currentTarget).append('<h1 class=gamepiece>X</h1>')
      let index = $(event.currentTarget).attr('value')
+     $(event.currentTarget).on('click',stopProp)
+     $(event.currentTarget).off('click')
      flipBoard(state, index)
      changeTurns(state)
      checkWinner(state)
      displayWinner(state)
+     tieGame(state)
+     lockButtons(state,stopProp)
+
    }
    else if(state.turn === 1) {
+     state.count++
      $(event.currentTarget).removeClass('white')
      $(event.currentTarget).addClass('blue')
+     $(event.currentTarget).append('<h1 class=gamepiece>O</h1>')
      let index = $(event.currentTarget).attr('value')
+     $(event.currentTarget).on('click',stopProp)
+     $(event.currentTarget).off('click');
      flipBoard(state, index)
      changeTurns(state)
      checkWinner(state)
      displayWinner(state)
+     tieGame(state)
+     lockButtons(state,stopProp)
    }
  })
  $('.reset').click(function(event) {
-   resetGame(state)
-   $('.square').addClass('white')
+   location.reload()
  })
-
 }$(eventListeners)
